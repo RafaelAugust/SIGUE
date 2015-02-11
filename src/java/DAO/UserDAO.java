83e820c84b9f6e3d3af437ddd652b98a8bfb3381
RafaelAugust <rafaleg001@hotmail.com>
2015-02-11 09:12:43 -0300

    classDAO

--- src/java/DAO/UserDAO.java
+++ src/java/DAO/UserDAO.java
@@ -6,16 +6,62 @@ package DAO;
 
 import Bean.HistoricoBean;
 import Bean.UserBean;
+import Utilitario.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author Familia
+ * @author Rafael Augusto 
+ * 
  */
 public class UserDAO {
-   
-    public boolean CadastrarAluno(){
+    
+    private Connection conexao;
+    public String CadastrarAluno;
+    public String DeletarAluno;
+    public String ListarAluno;
+    public String AlterarAluno;
+    public String MostrarHistorico;
+    public String AlterarHistorico;
+    public String DeletarHistorico;
+    
+    public  UserDAO(){
+        Conexao con = new Conexao();
+        this.conexao = con.GetConnection();
         
+        CadastrarAluno = "INSERT INTO alunos(nome, sobrenome, datnasc, sexo, idade, pai, mae)"
+                        + " VALUES('?','?','?','?','?','?','?');";
+        DeletarAluno = "";
+        ListarAluno = "";
+        AlterarAluno = "";
+        MostrarHistorico = "";
+        AlterarHistorico = "";
+        DeletarHistorico = "";
+    }
+   
+    public boolean CadastrarAluno(UserBean Aluno){
+        try
+        {
+            if(conexao != null)
+            {
+                PreparedStatement stm = conexao.prepareStatement(CadastrarAluno);
+                 stm.setString(1, Aluno.getNome());
+                 
+                 conexao.close(); 
+                 stm.close();
+            }else
+            {
+             JOptionPane.showMessageDialog(null,"Erro Conexao null", null, JOptionPane.INFORMATION_MESSAGE);
+            }
+           
+        }
+        catch(Exception erro)
+        {
+            JOptionPane.showMessageDialog(null,"Erro De Catch Exception", null, JOptionPane.INFORMATION_MESSAGE);
+        }
         
     return false;
     }
